9a9371e72ccd8de1581bf789c4ac3b23a59803f4
NikitaTrushinQA <106978180+NikitaTrushinQA@users.noreply.github.com>
2022-11-15 16:38:55 +0300

    add class ProductPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,12 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.ID,'login_form')
     LOGIN_RESTRATION_FORM = (By.ID,'register_form')
+
+class ProductPageLocators():
+    PRODUCT_PAGE_ADD_TO_CART_BUTTON=(By.CLASS_NAME,'btn.btn-lg.btn-primary.btn-add-to-basket')
+    NAME_OF_PRODUCT = (By.CSS_SELECTOR,'.col-sm-6.product_main h1')
+    NAME_OF_PRODUCT_IN_MESSAGE=(By.CSS_SELECTOR,'#messages .alert-success .alertinner strong')
+    MESSAGE_ABOUT_ADDING_PRODUCT =(By.CSS_SELECTOR,'#messages .alert-success .alertinner ')
+    PRODUCT_PRICE =(By.CSS_SELECTOR,'.col-sm-6.product_main .price_color')
+    CART_PRICE = (By.CSS_SELECTOR,'.alertinner p strong')
+    MESSAGE_ABOUT_CART_PRICE =(By.CSS_SELECTOR,'.alertinner p:first-child')
