3c164d5a3c45ca63fd8f2867ad6fbaf74827dba8
NikitaTrushinQA <106978180+NikitaTrushinQA@users.noreply.github.com>
2022-11-16 15:46:29 +0300

    change tabs in base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -18,7 +18,7 @@ class BasePage():
             return False
         return True
     
-        def solve_quiz_and_get_code(self):
+    def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
         answer = str(math.log(abs((12 * math.sin(float(x))))))
