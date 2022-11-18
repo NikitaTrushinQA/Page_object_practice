c2cc4d9168d8b1dcadf948f7e8003356c6d481d3
NikitaTrushinQA <106978180+NikitaTrushinQA@users.noreply.github.com>
2022-11-18 17:32:04 +0300

    add basket_page.py

---
+++ pages/basket_page.py
@@ -0,0 +1,32 @@
+from .base_page import BasePage
+from .locators import BasketPageLocators
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+
+class BasketPage(BasePage):
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
+    def should_not_be_items_in_basket(self):
+        '''Проверка,что в корзине нет товаров'''
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS), "Basket is not empty"
+
+
+
+
+
