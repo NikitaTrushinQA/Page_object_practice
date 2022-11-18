7d04d9351a15218bdc79020b9d6edacc5b2d5ca5
NikitaTrushinQA <106978180+NikitaTrushinQA@users.noreply.github.com>
2022-11-18 17:37:26 +0300

    add 2 classes to locators.py
    BasePageLocators and BasketPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -17,3 +17,12 @@ class ProductPageLocators():
     PRODUCT_PRICE =(By.CSS_SELECTOR,'.col-sm-6.product_main .price_color')
     CART_PRICE = (By.CSS_SELECTOR,'.alertinner p strong')
     MESSAGE_ABOUT_CART_PRICE =(By.CSS_SELECTOR,'.alertinner p:first-child')
+    
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR,'.btn-group' )
+
+class BasketPageLocators():
+    BASKET_ITEMS = (By.CSS_SELECTOR,'.basket_summary ')
+
