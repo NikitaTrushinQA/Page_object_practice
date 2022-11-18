c041b6b1ee18a9e8148ac92f9b06c552330534a3
NikitaTrushinQA <106978180+NikitaTrushinQA@users.noreply.github.com>
2022-11-18 17:48:16 +0300

    full update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,7 @@
 from .pages.product_page import ProductPage
 import time
 import pytest
-
-
+from .pages.login_page import LoginPage
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -14,13 +13,13 @@ import pytest
                                     pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+@pytest.mark.skip
 def test_guest_can_add_product_to_cart(browser,link):
     '''Проверки: добавление товара в корзину,
                  наличие сообщения о добавлении товара в корзину,
                  имя добавленного товара соответствует имени в корзине,
                  наличие сообщения со стоимостью корзины,
                  cтоимость корзины совпадает с ценой товара'''
-    #link = ' http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7'
     page = ProductPage(browser, link)
     page.open()
     page.add_to_cart()
@@ -30,4 +29,40 @@ def test_guest_can_add_product_to_cart(browser,link):
     page.should_be_message_with_total_cart_price()
     page.check_total_price_of_cart()
 
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    '''Проверка,отсутствия сообщения об успешном добавлении товара в корзину,после добавления товара в корзину'''
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart()
+    page.should_not_be_success_message()
 
+def test_guest_cant_see_success_message(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart()
+    time.sleep(1)
+    page.success_message_disappeared()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
