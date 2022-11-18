92c182c2c67a963677c4c3214ff28d7df4976868
NikitaTrushinQA <106978180+NikitaTrushinQA@users.noreply.github.com>
2022-11-18 17:44:22 +0300

    update test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,7 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
+import pytest
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -18,3 +20,12 @@ def test_should_be_login_page(browser):
     page =LoginPage(browser,link)
     page.open()
     page.should_be_login_page()
+
+@pytest.mark.new
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()                                             #Гость открывает главную страницу
+    page.go_to_basket()                                     #Переходит в корзину по кнопке в шапке сайта
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_items_in_basket()                    #Ожидаем, что в корзине нет товаров
