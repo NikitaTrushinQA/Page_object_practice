7987658a129950b35482f127a68c405bf5e73956
NikitaTrushinQA <106978180+NikitaTrushinQA@users.noreply.github.com>
2022-12-01 15:22:05 +0300

    fix tabs

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,6 +5,8 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from .locators import BasePageLocators
+
+
 class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
