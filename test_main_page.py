ea130e18b223d5b191924d154486290e1e856828
NikitaTrushinQA <106978180+NikitaTrushinQA@users.noreply.github.com>
2022-12-01 15:18:21 +0300

    fix tabs and pep8 misstakes

--- test_main_page.py
+++ test_main_page.py
@@ -1,31 +1,35 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
-import pytest
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
 
 def test_should_be_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page =LoginPage(browser,link)
+    page = LoginPage(browser, link)
     page.open()
     page.should_be_login_page()
 
-@pytest.mark.new
+
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/"
     page = MainPage(browser, link)
-    page.open()                                             #Гость открывает главную страницу
-    page.go_to_basket()                                     #Переходит в корзину по кнопке в шапке сайта
+    page.open()
+    page.go_to_basket()
     basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_not_be_items_in_basket()                    #Ожидаем, что в корзине нет товаров
+    basket_page.should_not_be_items_in_basket()
+    basket_page.should_be_text_that_basket_is_empty()
+
