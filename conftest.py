5e08be25ba4d2a9d626e96feeaca964574d5a681
NikitaTrushinQA <106978180+NikitaTrushinQA@users.noreply.github.com>
2022-12-01 15:15:44 +0300

    fix tabs and pep8 misstakes

--- conftest.py
+++ conftest.py
@@ -1,18 +1,17 @@
-
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import time
+
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="ru",help="Choose language: ru,en,ec ...")
+    parser.addoption('--language', action='store', default="ru", help="Choose language: ru,en,ec ...")
+
 
 @pytest.fixture(scope="function")
 def browser(request):
     user_language = request.config.getoption("language")
     browser = None
-
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     print("\nstart chrome browser for test..")
@@ -20,5 +19,6 @@ def browser(request):
     yield browser
     print("\nquit browser..")
     browser.quit()
+
 
 
